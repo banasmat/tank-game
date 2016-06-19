316b180845d8d82d33aa7ca129a5b69e5aa107ad
jorm <banasmat@gmail.com>
2016-06-19 12:48:21 +0200

    Temporarily fixed background scrolling new position glitch, removed inheritance

--- Assets/Scripts/Background/BackgroundMovement.cs
+++ Assets/Scripts/Background/BackgroundMovement.cs
@@ -4,63 +4,66 @@ using System.Collections;
 public class BackgroundMovement : MonoBehaviour {
 
 	public GameObject gameObjectPrefab;
+	public float reversedScrollSpeed; // The higher decimal, the slower movement
+	public float groundYPositionDifference; // Y position in relation to the ground level
 
-	protected float scrollSpeed;
 	protected float cameraWidth;
-	protected float cameraRightBoundPosition;
-	protected float spriteRightBoundPosition;
+	protected float cameraRightEdgePosition;
+	protected float spriteRightEdgePosition;
 	protected Vector3 startPosition;
 	protected GameObject ground;
 	protected Sprite sprite;
 	protected Sprite groundSprite;
 	protected Transform groundTransform;
 	protected bool cloned;
+	protected float yPosition;
+	protected float spriteSizeHalf;
+	protected float cameraWidthDoubled;
 
 	void Awake ()
 	{
-		
 		sprite = GetComponent<SpriteRenderer> ().sprite;
 
 		ground = GameObject.FindGameObjectWithTag (TagContainer.GROUND);
 		groundSprite = ground.GetComponent<SpriteRenderer> ().sprite;
 		groundTransform = ground.GetComponent<Transform> ();
-		scrollSpeed = GetScrollSpeed() ;
 		cloned = false;
 		cameraWidth = Camera.main.orthographicSize;
+
+		spriteSizeHalf = sprite.bounds.size.x / 2;
+		cameraWidthDoubled = cameraWidth * 2;
+
+		yPosition = groundTransform.position.y + groundYPositionDifference;
 	}
 
 	void Start(){
-		startPosition = new Vector3(transform.position.x, transform.position.y);
+		startPosition = new Vector3(transform.position.x, yPosition);
 	}
 
 	//TODO optimize move some calculations to variables
 	void FixedUpdate ()
 	{
-		transform.position = new Vector3(startPosition.x + Camera.main.transform.position.x * scrollSpeed, transform.position.y);
+		transform.position = new Vector3(startPosition.x + Camera.main.transform.position.x * reversedScrollSpeed, yPosition);
 
-		cameraRightBoundPosition = Camera.main.transform.position.x + cameraWidth;
-		spriteRightBoundPosition = transform.position.x + sprite.bounds.size.x / 2;
+		cameraRightEdgePosition = Camera.main.transform.position.x + cameraWidth;
+		spriteRightEdgePosition = transform.position.x + spriteSizeHalf;
 
-		float differenceAhead = spriteRightBoundPosition - cameraRightBoundPosition;
+		float differenceAhead = spriteRightEdgePosition - cameraRightEdgePosition;
 
 		// If image end is close to the camera right bound, create a new one
-		if (differenceAhead < cameraWidth * 2 && differenceAhead > 0) {
+		if (differenceAhead < cameraWidthDoubled && differenceAhead > 0) {
 			if (false == cloned) {
-				// Make sure that new object is intersected a bit with the old one
-				//FIXME When Camera.main.transform.position.x gets higher, new object's x is too low
-				GameObject clonedBackground = Instantiate (this.gameObjectPrefab, new Vector3(spriteRightBoundPosition + (sprite.bounds.size.x / 2) - Camera.main.transform.position.x * scrollSpeed - 0.1f, transform.position.y), transform.rotation) as GameObject;
+				// Make sure that new tile is intersected a bit with the old one ( - 0.1f )
+				//TODO When Camera.main.transform.position.x gets higher, new object's x is too low. Temporarily fixed with changing Y position ( yPosition - 10 )
+				GameObject clonedBackground = Instantiate (this.gameObjectPrefab, new Vector3(spriteRightEdgePosition + spriteSizeHalf - Camera.main.transform.position.x * reversedScrollSpeed - 0.1f, yPosition - 10), transform.rotation) as GameObject;
 				cloned = true;
 			}
 		}
 
 		// If sprite is left behind the camera, destroy it
-		if ((Camera.main.transform.position.x - cameraWidth - spriteRightBoundPosition) > cameraWidth * 2) {
+		if ((Camera.main.transform.position.x - cameraWidth - spriteRightEdgePosition) > cameraWidthDoubled) {
 			Destroy (gameObject);
 		}
-	}
-
-	protected virtual float GetScrollSpeed(){
-		return 0;
 	}
 }
 
