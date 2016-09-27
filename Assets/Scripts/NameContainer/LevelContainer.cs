bf031c9fac71bc27e82ee0b8b5bb0bb0a3937634
Mateusz Banaś <m.banas@createit.pl>
2016-09-27 15:22:39 +0200

    Implemented basic menu ui

--- Assets/Scripts/NameContainer/LevelContainer.cs
+++ Assets/Scripts/NameContainer/LevelContainer.cs
@@ -1,4 +1,10 @@
 ﻿class LevelContainer
 {
-    public const string TEST = "TestScene";
+    public static readonly string[] LEVEL = new string[1] {
+        "TestScene" //0
+    };
+        
+        
+    public const string MAIN_MENU = "MainMenu";
+    public const string LEVELS_MENU = "LevelsMenu";
 }
---
+++ Assets/Scripts/UI/MenuButtonsScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles Menu UI buttons
+/// </summary>
+public class MenuButtonsScript : MonoBehaviour {
+
+
+    #region These methods are connected with buttons in the Editor
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(LevelContainer.LEVELS_MENU);
+    }
+
+    public void BackToMainMenuButton()
+    {
+        SceneManager.LoadScene(LevelContainer.MAIN_MENU);
+    }
+
+    public void LoadLevel(int level)
+    {
+        SceneManager.LoadScene(LevelContainer.LEVEL[level]);
+    }
+
+    #endregion
+}
