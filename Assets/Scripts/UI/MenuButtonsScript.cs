f604eadd12eea9dac7229f7d977a5996f2befd0f
Mateusz Banaś <m.banas@createit.pl>
2016-09-28 15:43:39 +0200

    Connected restart level and resume buttons

--- Assets/Scripts/UI/MenuButtonsScript.cs
+++ Assets/Scripts/UI/MenuButtonsScript.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -8,19 +9,42 @@ public class MenuButtonsScript : MonoBehaviour {
 
 
     #region These methods are connected with buttons in the Editor
+
     public void PlayButton()
     {
         SceneManager.LoadScene(LevelContainer.LEVELS_MENU);
     }
 
-    public void BackToMainMenuButton()
+    public void BackToMainMenuButton(bool showPopup)
     {
+        if(true == showPopup)
+        {
+            //TODO are you sure? popup
+        }
         SceneManager.LoadScene(LevelContainer.MAIN_MENU);
     }
 
     public void LoadLevel(int level)
     {
         SceneManager.LoadScene(LevelContainer.LEVEL[level]);
+    }
+
+    public void RestartLevelButton()
+    {   
+        //TODO are you sure? popup
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        int level = Array.FindIndex(LevelContainer.LEVEL, value => value.Equals(sceneName));
+
+        SceneManager.LoadScene(LevelContainer.LEVEL[level]);
+    }
+
+    /// <summary>
+    /// Disables pause menu
+    /// </summary>
+    public void ResumeButton()
+    {
+        GameObject.Find(NameContainer.PAUSE_MENU_PANEL).SetActive(false);
     }
 
     #endregion
