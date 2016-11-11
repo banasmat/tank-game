ae9fd48d9b836db1553dd6f68041c673d52ba0e4
Mateusz Banaś <m.banas@createit.pl>
2016-11-11 14:20:03 +0100

    Experimental cylinder background, compressed background sprites (60 fps)

---
+++ Assets/Scripts/Background/CylinderBackgroundMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderBackgroundMovement : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    private PlayerMovement playerMovement;
+    private float reversedScrollSpeed;
+    private float initialPositionZ;
+    private float initialPositionY;
+
+    // Use this for initialization
+    void Start () {
+        playerMovement = GameObject.Find(NameContainer.PLAYER).GetComponent<PlayerMovement>();
+        reversedScrollSpeed = 1 * scrollSpeed / 100;
+
+        initialPositionZ = transform.position.z;
+        initialPositionY = transform.position.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Camera.main.transform.position.x, initialPositionY, initialPositionZ);
+
+        // Blender vertical axis is Z (Unity: Y)
+        transform.Rotate(new Vector3(0, 0, playerMovement.Velocity.x * reversedScrollSpeed));
+    }
+}
